58cfe1f3ab2fd14eefc6c68114c6f2246b59302f
Barabuba <barabuba@mail.ru>
2014-04-21 10:31:47 +0300

    Lesson 7 problem check

---
+++ addressbook-native-tests/src/com/example/fw/ApplicationManager.java
@@ -0,0 +1,56 @@
+package com.example.fw;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ApplicationManager {
+	
+
+	private ContactHelper contactHelper;
+	private Properties props;
+
+	private Object processHelper;
+
+	
+	
+	public void start() throws IOException {
+		getProcessHelper().startAppUnderTest();	
+	 }
+	
+	public void stop() {
+		getProcessHelper().stopAppUnderTest();	
+	 }
+	
+	public void setProperties(Properties props) {
+	this.props = props;	
+	}
+	
+	public String getProperty(String key) {
+		return props.getProperty(key);
+	}
+	
+	public String getProperty(String key, String defaultValue) {
+		return props.getProperty(key, defaultValue);
+	}
+
+	public ContactHelper getContactHelper() {
+		if (contactHelper == null) {
+			contactHelper = new ContactHelper(this);
+		}
+		return (ContactHelper) contactHelper;
+	}
+	
+	public ProcessHelper getProcessHelper() {
+		if (processHelper == null) {
+			processHelper = new ProcessHelper(this);
+		}
+		return (ProcessHelper) processHelper;
+	}
+}
+	
+	
+	
+
+	
+
+
